09dd71e1893aa6578bb7dbcd602b9fe79e36342d
nawan44 <rachmat.d.gunawan@gmail.com>
2020-07-26 23:47:12 +0700

    search-bar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Submissio-Dicoding-FrontEnd
-# Submissio-Dicoding-FrontEnd
+# Submission-Dicoding-FrontEnd
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>Asynchronous JavaScript Request</li>
+<li>Javascript ES6</li>
+<li>Web Components</li>
+<li>Node Package Manager</li>
+<li>Webpack</li>
+
+</ul>
---
+++ src/script/view/main.js
@@ -0,0 +1,43 @@
+import '../component/search-bar.js';
+import DataSource from '../data/data-source.js';
+ 
+const main = () => {
+   const searchElement = document.querySelector("search-bar");
+   const clubListElement = document.querySelector("#clubList");
+ 
+   const onButtonSearchClicked = async () => {
+       try {
+           const result = await DataSource.searchClub(searchElement.value);
+           renderResult(result);
+       } catch (message) {
+           fallbackResult(message)
+       }
+   };
+ 
+   const renderResult = results => {
+       clubListElement.innerHTML = "";
+       results.forEach(club => {
+           const { name, fanArt, description } = club;
+           const clubElement = document.createElement("div");
+           clubElement.setAttribute("class", "club");
+ 
+           clubElement.innerHTML = `
+               <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+               <div class="club-info">
+                   <h2>${name}</h2>
+                   <p>${description}</p>
+               </div>`;
+ 
+           clubListElement.appendChild(clubElement);
+       })
+   };
+ 
+   const fallbackResult = message => {
+       clubListElement.innerHTML = "";
+       clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+   };
+ 
+   searchElement.clickEvent = onButtonSearchClicked;
+};
+ 
+export default main;
