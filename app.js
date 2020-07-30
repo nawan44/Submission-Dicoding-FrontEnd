58f6218a0eb140d248dfbcf13419473992459b2c
nawan44 <rachmat.d.gunawan@gmail.com>
2020-07-30 22:41:44 +0700

    app - navbar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import "./src/script/component/app-bar.js";
+// import "./src/script/component/app-bar.js";
 import "./src/script/component/com-carousel.js";
+import "./src/script/component/app-navbar.js";
 import "./src/script/component/app-footer.js";
 
 import main from "./src/script/view/main.js";
--- src/script/component/com-carousel.js
+++ src/script/component/com-carousel.js
@@ -1,21 +1,26 @@
 import Handwash from '../../image/handwash.jpg';
+import Co from '../../image/co.jpg';
+import Lockd from '../../image/lockdown.jpg'
+
 
 class ComCarousel extends HTMLElement{
     connectedCallback(){
         this.render();
     }
+
     render(){
         this.innerHTML=`   
         <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-interval="10000">
-            <img src='image/banner1.png' class="d-block w-100" alt="..." width="100%" height="500px">
+          <img src="${Handwash}" class="d-block w-100" alt="..." width="100%" height="500px">
           </div>
           <div class="carousel-item" data-interval="2000">
-            <img src="'+Handwash+' "class="d-block w-100" alt="..." width="100%" height="500px">
+          <img src="${Co}" class="d-block w-100" alt="..." width="100%" height="500px">
+
           </div>
           <div class="carousel-item">
-            <img src='image/handwash.jpg' class="d-block w-100" alt="..." width="100%" height="500px">
+          <img src="${Lockd}" class="d-block w-100" alt="..." width="100%" height="500px">
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
