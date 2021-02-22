6932f167d8ab0edf438f8a951bf8e9460df45f7f
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-02-22 22:53:48 +0700

    build

--- src/script/component/app-navbar.js
+++ src/script/component/app-navbar.js
@@ -16,7 +16,7 @@ class AppNavbar extends HTMLElement {
   render() {
     this.innerHTML = `  
         <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color:#E8ECEF;" >
-        <a class="navbar-brand" href="#" style="color:blue; font-weight:bold; font-size:25px">Covid -19</a>
+        <a class="navbar-brand"  onclick="setTimeout(location.reload.bind(location), 1)" href="#" style="color:blue; font-weight:bold; font-size:25px">Covid -19</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
