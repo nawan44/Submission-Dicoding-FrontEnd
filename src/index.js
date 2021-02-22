285e747f2471a19d37553139cfad0525a61952d6
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-02-22 20:34:20 +0700

    build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import babelParser from "@babel/parser";
 
 import "./script/component/com-carousel.js";
 import "./script/component/app-navbar.js";
 import "./script/component/app-footer.js";
 import "./script/component/page/About.js";
 import "./script/component/page/Error.js";
+// import babelParser from "@babel/parser";
 
 import "./styles/style.css";
 import main from "./script/view/main.js";
