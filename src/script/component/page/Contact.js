4bbebeb58ebe6de085cc415ffece2f9ead2078f7
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-13 23:58:44 +0700

    update contact

--- src/script/component/page/Contact.js
+++ src/script/component/page/Contact.js
@@ -4,9 +4,8 @@ class Contact extends HTMLElement{
         <div class="jumbotron jumbotron-fluid">
         <div class="container">
         
-        <h1 class="display-4">WHO Headquarters in Geneva   </h1>
-        <h3>
-        </h3>
+        <h3 class="display-4">WHO Headquarters in Geneva   </h3>
+        
         
         <p class="lead">
             Avenue Appia 20 <br />
@@ -16,6 +15,22 @@ class Contact extends HTMLElement{
         <br/>
            <p>Source : https://www.who.int/</p>
         </div>
+<br/>
+        <div class="container">
+        
+        <h3 class="display-4">Ministry of Health of the Republic of Indonesia </h3>
+      
+        
+        <p class="lead">
+        Gedung Sujudi, H. R. Rasuna Said Street No.Kav 4 - 9, Kuningan, South Jakarta City<br />
+        Jakarta 12950 <br />
+            Telephone: + 62 21 5265043<br />
+        </p>
+        <br/>
+           <p>Source : https://kemkes.go.id/</p>
+        </div>
+
+
         </div>
                `;
 
